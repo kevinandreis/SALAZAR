c2e9141e0a1d5a3705a5729406a2915bc5f1c701
kevinandreis <kevinandrei2321@gmail.com>
2024-10-20 20:23:24 +0800

    Update handlesforms.php

--- handlesforms.php
+++ handlesforms.php
@@ -1,96 +1,69 @@
-<?php 
+<?php
+require_once 'core/dbConfig.php';
+require_once 'core/models.php';
 
-require_once 'dbConfig.php'; 
-require_once 'models.php';
+$error = '';
+$successMessage = '';
 
-if (isset($_POST['insertBtn'])) {
-	$firstName = trim($_POST['firstName']);
-	$lastName = trim($_POST['lastName']);
-	$birthdate = trim($_POST['birthdate']);
-	$age = trim($_POST['age']);
-	$address = trim($_POST['address']);
-	$position = trim($_POST['position']);
-	$salary = trim($_POST['salary]);
+// Handle insertion
+if (isset($_POST['insertNewEngineerBtn'])) {
+    $firstName = trim($_POST['firstName']);
+    $lastName = trim($_POST['lastName']);
+    $birthdate = trim($_POST['birthdate']);
+    $age = trim($_POST['age']);
+    $address = trim($_POST['address']);
+    $position = trim($_POST['position']);
+    $salary = trim($_POST['salary']);
 
-	if (!empty($firstName) && !empty($lastName) && !empty($gbirthdate) && !empty($age) && !empty($address)  && !empty($position)  && !empty($salary)) {
-			$query = insertIntoRecords($pdo, $firstName, $lastName, $birthdate, $age, $address, $position, $salary);
-
-		if ($query) {
-			header("Location: ../index.php");
-		}
-
-		else {
-			echo "Insertion failed";
-		}
-	}
-
-	else {
-		echo "Make sure that no fields are empty";
-	}
-	
+    if (!empty($firstName) && !empty($lastName) && !empty($birthdate) && !empty($age) && !empty($address) && !empty($position) && !empty($salary)) {
+        $query = insertIntoEngineerRecords($pdo, $firstName, $lastName, $birthdate, $age, $address, $position, $salary);
+       
+        if ($query) {
+            $successMessage = "Software Engineer registered successfully!";
+        } else {
+            $error = "Insertion failed";
+        }
+    } else {
+        $error = "Make sure that no fields are empty";
+    }
 }
 
+// Handle update
+if (isset($_POST['editEngineerBtn'])) {
+    $engineer_id = $_GET['engineer_id'];
+    $firstName = trim($_POST['firstName']);
+    $lastName = trim($_POST['lastName']);
+    $birthdate = trim($_POST['birthdate']);
+    $age = trim($_POST['age']);
+    $address = trim($_POST['address']);
+    $position = trim($_POST['position']);
+    $salary = trim($_POST['salary']);
 
-if (isset($_POST['editBtn'])) {
-    $id = $_POST['id'];
-	$firstName = trim($_POST['firstName']);
-	$lastName = trim($_POST['lastName']);
-	$birthdate = trim($_POST['birthdate']);
-	$age = trim($_POST['age']);
-	$address = trim($_POST['address']);
-	$position = trim($_POST['position']);
-	$salary = trim($_POST['salary]);
-	
-	echo "ID: $id<br>";
-    echo "First Name: $firstName<br>";
-    echo "Last Name: $lastName<br>";
-    echo "Birthdate: $birthdate<br>";
-    echo "Age: $age<br>";
-    echo "Address: $address<br>";
-    echo "Position: $expectedsalary<br>";
-	echo "salary: $salary<br>";
-
-	if (!empty($firstName) && !empty($lastName) && !empty($gbirthdate) && !empty($age) && !empty($address)  && !empty($position)  && !empty($salary)) {
-
-		$query = insertIntoRecords($pdo, $firstName, $lastName, $birthdate, $age, $address, $position, $salary);
-
-		if ($query) {
-			header("Location: ../index.php");
-		}
-		else {
-			echo "Update failed";
-		}
-
-	}
-
-	else {
-		echo "Make sure that no fields are empty";
-	}
-
+    if (!empty($engineer_id) && !empty($firstName) && !empty($lastName) && !empty($birthdate) && !empty($age) && !empty($address) && !empty($position) && !empty($salary)) {
+        $query = updateAnEngineer($pdo, $engineer_id, $firstName, $lastName, $birthdate, $age, $address, $position, $salary);
+       
+        if ($query) {
+            $successMessage = "Software Engineer updated successfully!";
+            header("Location: ../sql/index.php");
+            exit();
+        } else {
+            $error = "Update failed";
+        }
+    } else {
+        $error = "Make sure that no fields are empty";
+    }
 }
 
-
-
-
-
-if (isset($_POST['deletetBtn'])) {
-
-	$query = deleterec($pdo, $_GET['id']);
-
-	if ($query) {
-		header("Location: ../index.php");
-	}
-	else {
-		echo "Deletion failed";
-	}
+if (isset($_POST['deleteEngineerBtn'])) {
+    $query = deleteAnEngineer($pdo, $_GET['engineer_id']);
+    if ($query) {
+        $successMessage = "Software Engineer deleted successfully!";
+        header("Location: ../sql/index.php");
+        exit();
+    } else {
+        $error = "Deletion failed";
+    }
 }
 
-
-
-
+$seeAllEngineerRecords = seeAllEngineerRecords($pdo);
 ?>
-
-
-
-
-
