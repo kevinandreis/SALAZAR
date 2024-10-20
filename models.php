f6c4e696dffe8dacc84609b300aaeb99e67cd63e
kevinandreis <kevinandrei2321@gmail.com>
2024-10-20 20:35:05 +0800

    Update models.php

--- models.php
+++ models.php
@@ -1,73 +1,33 @@
-<?php 
-
+<?php
 require_once 'dbConfig.php';
 
-function insertIntoRecords($pdo,$first_name, $last_name, $birthdate, $age, $address, $position, $salary) {
-
-	$sql = "INSERT INTO employees (first_name,last_name,birthdate,age,address,position,salary) VALUES (?,?,?,?,?,?,?)";
-
-	$stmt = $pdo->prepare($sql);
-
-	$executeQuery = $stmt->execute([($pdo,$first_name, $last_name, $birthdate, $age, $address, $position, $salary)]);
-
-	if ($executeQuery) {
-		return true;	
-	}
+function insertIntoEngineerRecords($pdo, $first_name, $last_name, $birthdate, $age, $address, $position, $salary) {
+    $sql = "INSERT INTO Engineers (FirstName, LastName, Birthdate, Age, Address, Position, Salary) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$first_name, $last_name, $birthdate, $age, $address, $position, $salary]);
 }
 
-function seeAllRecords($pdo) {
-	$sql = "SELECT * FROM employees";
-	$stmt = $pdo->prepare($sql);
-	$executeQuery = $stmt->execute();
-	if ($executeQuery) {
-		return $stmt->fetchAll();
-	}
+function seeAllEngineerRecords($pdo) {
+    $sql = "SELECT * FROM Engineers";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute() ? $stmt->fetchAll() : [];
 }
 
-function getID($pdo, $id) {
-	$sql = "SELECT * FROM employees WHERE id = ?";
-	$stmt = $pdo->prepare($sql);
-	if ($stmt->execute([id])) {
-		return $stmt->fetch();
-	}
+function getEngineerByID($pdo, $engineer_id) {
+    $sql = "SELECT * FROM Engineers WHERE EngineerID = ?";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$engineer_id]) ? $stmt->fetch() : null;
 }
 
-function updateRecord($pdo, $first_name, $last_name, $birthdate, $age, $address, $position, $salary) {
-
-	$sql = "UPDATE employees 
-				SET first_name = ?, 
-					last_name = ?, 
-					birthdate = ?, 
-					age = ?, 
-					address= ?, 
-					position = ?, 
-					salary = ? 
-			WHERE id = ?";
-	$stmt = $pdo->prepare($sql);
-	
-	$executeQuery = $stmt->execute([$first_name, $last_name, $birthdate, $age, $address, $position, $salary]);
-
-	if ($executeQuery) {
-		return true;
-	}
+function updateAnEngineer($pdo, $engineer_id, $first_name, $last_name, $birthdate, $age, $address, $position, $salary) {
+    $sql = "UPDATE Engineers SET FirstName = ?, LastName = ?, Birthdate = ?, Age = ?, Address = ?, Position = ?, Salary = ? WHERE EngineerID = ?";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$first_name, $last_name, $birthdate, $age, $address, $position, $salary, $engineer_id]);
 }
 
-
-
-function deleteRecord($pdo, $id) {
-
-	$sql = "DELETE FROM employees WHERE id = ?";
-	$stmt = $pdo->prepare($sql);
-
-	$executeQuery = $stmt->execute([$id]);
-
-	if ($executeQuery) {
-		return true;
-	}
-
+function deleteAnEngineer($pdo, $engineer_id) {
+    $sql = "DELETE FROM Engineers WHERE EngineerID = ?";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$engineer_id]);
 }
-
-
-
-
 ?>
